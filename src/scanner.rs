c6dab89ecabdcecf71f126dae5d6055f525b5200
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-04-23 10:02:20 +0545

    update scanner to be more like implementation in the book * add properties like current and start to track the characters being scanned * add has_error flag to identify if any error encountered during scanning * add helper functions like scan_token, is_at_end , add_token and advance like given in the book * report each error during scanning immediately instead of collecting them and reporting later

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,73 +1,81 @@
-use crate::error::{Error, report_error};
+use crate::error::{LoxError, report_error};
 use crate::token::{LiteralType, Token, TokenType};
 pub struct Scanner {
-    source: String,
-    pub tokens: Vec<Token>,
-    current_line: usize,
+    source: Vec<char>,
+    tokens: Vec<Token>,
+    line: usize,
+    start: usize,
+    current: usize,
+    has_error: bool,
 }
 impl Scanner {
     pub fn new(source: String) -> Self {
         let tokens: Vec<Token> = Vec::new();
         Self {
-            source,
+            source: source.chars().collect(),
             tokens,
-            current_line: 1,
+            line: 1,
+            start: 0,
+            current: 0,
+            has_error: false,
         }
     }
-    pub fn scan_tokens(&mut self) -> Result<(), ()> {
-        self.current_line = 0;
-        let mut errors: Vec<Error> = Vec::new();
-        for c in self.source.clone().chars() {
-            match c {
-                '(' => {
-                    self.add_token(TokenType::LeftParen, String::from(c), None);
-                }
-                ')' => {
-                    self.add_token(TokenType::RightParen, String::from(c), None);
-                }
-                '{' => {
-                    self.add_token(TokenType::LeftBrace, String::from(c), None);
-                }
-                '}' => {
-                    self.add_token(TokenType::Rightbrace, String::from(c), None);
-                }
-                '.' => {
-                    self.add_token(TokenType::Dot, String::from(c), None);
-                }
-                ',' => {
-                    self.add_token(TokenType::Comma, String::from(c), None);
-                }
-                ';' => {
-                    self.add_token(TokenType::Semicolon, String::from(c), None);
-                }
-                '+' => {
-                    self.add_token(TokenType::Plus, String::from(c), None);
-                }
-                '-' => {
-                    self.add_token(TokenType::Minus, String::from(c), None);
-                }
-                '*' => {
-                    self.add_token(TokenType::Star, String::from(c), None);
-                }
-                ' ' | '\r' | '\t' => continue,
-                '\n' => self.current_line += 1,
-                _ => errors.push(Error::UnexpectedChar {
-                    char: c,
-                    line: self.current_line,
-                }),
-            }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+    pub fn scan_tokens(&mut self) -> Result<&Vec<Token>, ()> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
         }
-        if errors.is_empty() {
-            Ok(())
-        } else {
-            for e in errors {
-                report_error(e);
-            }
+        self.add_token(TokenType::Eof, String::new(), LiteralType::Nil);
+
+        if self.has_error {
             Err(())
+        } else {
+            Ok(&self.tokens)
         }
     }
-    fn add_token(&mut self, token_type: TokenType, lexeme: String, literal: Option<LiteralType>) {
+
+    fn add_token(&mut self, token_type: TokenType, lexeme: String, literal: LiteralType) {
         self.tokens
-            .push(Token::new(token_type, lexeme, literal, self.current_line));
+            .push(Token::new(token_type, lexeme, self.line, literal));
+    }
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source[self.current - 1]
+    }
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '(' => self.add_token(TokenType::LeftParen, String::from(c), LiteralType::Nil),
+
+            ')' => self.add_token(TokenType::RightParen, String::from(c), LiteralType::Nil),
+
+            '{' => self.add_token(TokenType::LeftBrace, String::from(c), LiteralType::Nil),
+
+            '}' => self.add_token(TokenType::Rightbrace, String::from(c), LiteralType::Nil),
+
+            '.' => self.add_token(TokenType::Dot, String::from(c), LiteralType::Nil),
+
+            ';' => self.add_token(TokenType::Comma, String::from(c), LiteralType::Nil),
+
+            ',' => self.add_token(TokenType::Semicolon, String::from(c), LiteralType::Nil),
+
+            '+' => self.add_token(TokenType::Plus, String::from(c), LiteralType::Nil),
+
+            '-' => self.add_token(TokenType::Minus, String::from(c), LiteralType::Nil),
+
+            '*' => self.add_token(TokenType::Star, String::from(c), LiteralType::Nil),
+            '\n' => self.line += 1,  // increment current line number
+            ' ' | '\t' | '\r' => (), // skip whitespaces
+            _ => {
+                self.has_error = true;
+                report_error(LoxError::UnexpectedChar {
+                    char: c,
+                    line: self.line,
+                });
+            }
+        }
     }
 }
