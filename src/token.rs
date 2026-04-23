ac89f073ec90718ee7ae3329cb4e95e085f6eb78
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-04-23 10:07:19 +0545

    update token struct * don't make literal property optional * Add nil literal property instead * implement Display trait for Token * update literal type enum to match correct options with correct data inside

--- src/token.rs
+++ src/token.rs
@@ -1,36 +1,38 @@
+use std::fmt::Display;
+
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
+    literal: LiteralType,
     line: usize,
-    literal: Option<LiteralType>,
 }
 impl Token {
-    pub fn new(
-        token_type: TokenType,
-        lexeme: String,
-        literal: Option<LiteralType>,
-        line: usize,
-    ) -> Self {
-        Self {
+    pub fn new(token_type: TokenType, lexeme: String, line: usize, literal: LiteralType) -> Self {
+        Token {
             token_type,
             lexeme,
             line,
-            literal: None,
+            literal,
         }
     }
 }
-impl ToString for Token {
-    fn to_string(&self) -> String {
-        // format!("{} {} {}", self.token_type, self.lexeme, self.literal)
-        self.lexeme.clone()
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {:?} {:?}",
+            self.token_type, self.lexeme, self.literal
+        )
     }
 }
+#[derive(Debug, Clone)]
 pub enum LiteralType {
-    String,
-    Int,
-    Char,
-    Callable,
+    String(String),
+    Number(f64),
+    Boolean(bool),
+    Nil,
 }
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // Single-character tokens.
     LeftParen,
