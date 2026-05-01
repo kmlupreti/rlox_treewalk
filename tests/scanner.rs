5ab9a6228efd79d4d81ec8601e94ec728686dc28
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-05-01 16:35:42 +0545

    Add some tests for scanner module

---
+++ tests/scanner.rs
@@ -0,0 +1,67 @@
+use lox::{
+    scanner::Scanner,
+    token::{LiteralType, Token, TokenType},
+};
+
+#[test]
+fn should_skip_comments() {
+    let source = "// hello this is test
+(
+// another test
+*
+";
+    let mut scanner = Scanner::new(String::from(source));
+    let mut tokens = scanner
+        .scan_tokens()
+        .expect("scan tokens failed")
+        .into_iter();
+    println!("tokens: {:?}", tokens);
+    assert_eq!(
+        tokens.next().unwrap(),
+        &Token::new(TokenType::LeftParen, String::from("("), 2, LiteralType::Nil)
+    );
+    assert_eq!(
+        tokens.next().unwrap(),
+        &Token::new(TokenType::Star, String::from("*"), 4, LiteralType::Nil)
+    );
+    assert_eq!(
+        tokens.next().unwrap(),
+        &Token::new(TokenType::Eof, String::from("\0"), 5, LiteralType::Nil)
+    )
+}
+
+#[test]
+fn should_recognize_single_char_tokens() {
+    let source = "(){}.;,+-*";
+    let source_chars = source.chars();
+    let mut scanner = Scanner::new(String::from(source));
+    let mut tokens = scanner.scan_tokens().unwrap().into_iter();
+    for c in source_chars {
+        assert_eq!(
+            tokens.next().unwrap(),
+            &Token::new(
+                single_char_to_token_type(c),
+                String::from(c),
+                1,
+                LiteralType::Nil
+            )
+        );
+    }
+}
+
+fn single_char_to_token_type(c: char) -> TokenType {
+    match c {
+        '(' => TokenType::LeftParen,
+        ')' => TokenType::RightParen,
+        '{' => TokenType::LeftBrace,
+        '}' => TokenType::Rightbrace,
+        '.' => TokenType::Dot,
+        ';' => TokenType::Comma,
+        ',' => TokenType::Semicolon,
+        '+' => TokenType::Plus,
+        '-' => TokenType::Minus,
+        '*' => TokenType::Star,
+        '\0' => TokenType::Eof,
+        _ => TokenType::Nil,
+    }
+}
