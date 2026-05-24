dbc84d6081713e5bcb68e27df4fe42d8d890a7db
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-05-24 22:51:04 +0545

    Add new error type for unterminated string

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,15 @@
 pub enum LoxError {
     UnexpectedChar { char: char, line: usize },
     ParseError,
+    UnterminatedString { line: usize },
 }
 pub fn report_error(e: LoxError) {
     match e {
         LoxError::UnexpectedChar { char, line } => {
             eprintln!("[line: {}] Unexpected character {:?} found", line, char);
+        }
+        LoxError::UnterminatedString { line } => {
+            eprintln!("[line: {}] Unterminated string", line);
         }
         _ => {
             eprintln!("Unknown error occured")
