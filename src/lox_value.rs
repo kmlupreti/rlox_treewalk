7416ecead770a38a1d0d39fd6d6199042a9e5794
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-04 15:20:21 +0545

    Add method to parse number from given LoxValue if possible

--- src/lox_value.rs
+++ src/lox_value.rs
@@ -1,7 +1,25 @@
+use crate::error::LoxError;
+
 #[derive(Debug, PartialEq)]
 pub enum LoxValue {
     String(String),
     Number(f64),
     Boolean(bool),
     Null,
+}
+impl LoxValue {
+    pub fn parse_num(&self) -> Result<f64, LoxError> {
+        match self {
+            LoxValue::Number(n) => Ok(*n),
+            LoxValue::String(s) => match s.parse::<f64>() {
+                Ok(n) => Ok(n),
+                Err(_) => Err(LoxError::EvalError {
+                    msg: "failed to parse string {s} to number",
+                }),
+            },
+            _ => Err(LoxError::EvalError {
+                msg: "failed to parse the value into number",
+            }),
+        }
+    }
 }
