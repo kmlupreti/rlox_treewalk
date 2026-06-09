a0a29c4c77f6741d67be881f1bea08a9e81961e7
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-09 17:53:31 +0545

    add assignment expression

--- src/expresssion.rs
+++ src/expresssion.rs
@@ -1,5 +1,4 @@
 use crate::token::Token;
-use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Expr {
@@ -21,30 +20,8 @@ pub enum Expr {
     Variable {
         name: Token,
     },
-}
-impl Display for Expr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self {
-            Self::Literal { value } => {
-                let value = value.lexeme.clone();
-                write!(f, "{value}")
-            }
-            Self::Unary { operator, right } => {
-                let operator = operator.lexeme.clone();
-                write!(f, "({operator} {right})")
-            }
-            Self::Binary {
-                left,
-                operator,
-                right,
-            } => {
-                let operator = operator.lexeme.clone();
-                write!(f, "({operator} {left} {right})")
-            }
-            Self::Grouping { expr } => {
-                write!(f, "(group {expr})")
-            }
-            Self::Variable { name } => write!(f, "{name}"),
-        }
-    }
+    Assign {
+        name: Token,
+        value: Box<Expr>,
+    },
 }
